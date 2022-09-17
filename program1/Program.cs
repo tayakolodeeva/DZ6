913bf41638cd4e0d40c856c7942c26e0e081001f
Taya <tayakolodeeva@yandex.ru>
2022-09-17 15:42:29 +0300

    fix

---
+++ program1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Задайте размер массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+
+ArrayRandomNumbers(array);// Заполнение массива числами.Вывод созданного массива на экран.
+NumberOfPositive(array);// Подсчет чисел больше 0.
+
+void ArrayRandomNumbers(int[] array) 
+{
+    Console.WriteLine("Введите элементы массива (элемент -> enter):");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine()); 
+    }
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write(" ] - созданный массив, ");
+}
+
+void NumberOfPositive(int[]array)
+{
+    int positive = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            positive++;
+        } 
+    }
+Console.Write("("+positive+")-колличество чисел больше 0.");
+}
